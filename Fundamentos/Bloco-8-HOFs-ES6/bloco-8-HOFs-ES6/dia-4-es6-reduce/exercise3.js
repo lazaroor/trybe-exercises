2f672e77f80106047db77df182f8326c1d7056d5
lazaro <lazaroandriola@gmail.com>
2022-02-09 11:03:48 -0300

    Exercicio 3 ok

--- Fundamentos/Bloco-8-HOFs-ES6/bloco-8-HOFs-ES6/dia-4-es6-reduce/exercise3.js
+++ Fundamentos/Bloco-8-HOFs-ES6/bloco-8-HOFs-ES6/dia-4-es6-reduce/exercise3.js
@@ -1,2 +1,11 @@
 const books = require('../dia-4-es6-reduce/arrayApoio');
 
+const expectedResult = 43;
+
+//Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
+function averageAge() {
+  const numberOfAuthor = books.length;
+  const ageOfAuthors = books.reduce((acc, currElement) => acc += currElement.releaseYear - currElement.author.birthYear, 0);  return ageOfAuthors/numberOfAuthor;
+}
+
+console.log(averageAge());
