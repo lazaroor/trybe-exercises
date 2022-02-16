061a7d28350034483795807bce6c6aa9cbb43756
lazaro <lazaroandriola@gmail.com>
2022-02-16 14:52:30 -0300

    Exercicios React inicial

---
+++ Front-End/Bloco-10-intro-react/10.1-React-intro/about-app/src/App.js
@@ -0,0 +1,13 @@
+    import React from 'react';
+    import './App.css';
+    import About from './about';
+
+    function App() {
+      return (
+        <ol>
+          <li>teste</li>
+        </ol>
+      );
+    }
+
+    export default App;
---
+++ Front-End/Bloco-10-intro-react/10.1-React-intro/about-app/src/about.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class About extends React.Component{
+  render() {
+    return <div>
+      <h1>Lázaro Ramos</h1>
+      <p>Estudante de desenvolvimento Web da Trybe</p>
+    </div>
+  }
+}
+
+export default About
