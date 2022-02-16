ba2ddf84f812f74b0e8edf6a3b361b7a48b90cb1
lazaro <lazaroandriola@gmail.com>
2022-02-16 15:45:44 -0300

    Exercicio 3 completo

---
+++ Front-End/Bloco-10-intro-react/10.1-React-intro/exercicios/exercicio3/src/Content.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+];
+
+class Content extends React.Component {
+  render() {
+    return conteudos.map((element) => {
+      return <div key={element.conteudo}>
+        <h4>{`O conteúdo é: ${element.conteudo}`}</h4>
+        <p>{`status: ${element.status}`}</p>
+        <p>{`bloco: ${element.bloco}`}</p>
+      </div>
+    });
+  }
+}
+
+export default Content;
