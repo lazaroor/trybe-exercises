f43e8cc5e0f197a012a9753d81bda6628f6c8996
lazaro <lazaroandriola@gmail.com>
2022-02-21 15:50:06 -0300

    Exercicios 5 a 7, incrementar valor e mostrar no botao

--- Front-End/Bloco-10-intro-react/10.3-Estado-Eventos-React/fancy-buttons/src/App.js
+++ Front-End/Bloco-10-intro-react/10.3-Estado-Eventos-React/fancy-buttons/src/App.js
@@ -3,19 +3,43 @@ import React from "react";
 class App extends React.Component {
   constructor() {
     super()
-    this.handleClick = this.handleClick.bind(this);
+    this.handleButtonOne = this.handleButtonOne.bind(this);
+    this.handleButtonTwo = this.handleButtonTwo.bind(this);
+    this.handleButtonThree = this.handleButtonThree.bind(this);
+    this.state = {
+      clickNumberOne: 0,
+      clickNumberTwo: 0,
+      clickNumberThree: 0,
+    }
   }
 
-  handleClick() {
-    console.log('Clicou no botão!');
-    console.log(this);
+  handleButtonOne() {
+    // variável props obrigatória, coloca-se "_" para tirar a falta de declaração
+    this.setState((estadoAnterior, _props) => ({
+      clickNumberOne: estadoAnterior.clickNumberOne + 1,
+    }))
   }
+
+  handleButtonTwo() {
+    // variável props obrigatória, coloca-se "_" para tirar a falta de declaração
+    this.setState((estadoAnterior, _props) => ({
+      clickNumberTwo: estadoAnterior.clickNumberTwo + 1,
+    }))
+  }
+
+  handleButtonThree() {
+    // variável props obrigatória, coloca-se "_" para tirar a falta de declaração
+    this.setState((estadoAnterior, _props) => ({
+      clickNumberThree: estadoAnterior.clickNumberThree + 1,
+    }))
+  }
+
   render() {
     console.log(this);
     return <>
-      <button onClick={this.handleClick}>Botão 1</button>
-      <button onClick={this.handleClick}>Botão 2</button>
-      <button onClick={this.handleClick}>Botão 3</button>
+      <button onClick={this.handleButtonOne}>{this.state.clickNumberOne}</button>
+      <button onClick={this.handleButtonTwo}>{this.state.clickNumberTwo}</button>
+      <button onClick={this.handleButtonThree}>{this.state.clickNumberThree}</button>
     </>
   }
 }
